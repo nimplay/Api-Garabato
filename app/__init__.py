496d9371b7c7bfe7c42220e712831cc0e8646cdb
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-02-07 14:06:36 -0400

    active cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from flasgger import Swagger
 from app.routes.products import products_bp
 from app.routes.paypal import paypal_bp
 from app.config import config
+from flask_cors import CORS
 
 def create_app(env="development"):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config[env])  # Cargar configuración según el entorno
 
     # Inicializa Swagger para documentación automática
